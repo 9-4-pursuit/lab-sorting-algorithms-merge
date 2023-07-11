60cb19d8475322cc9314f57b8796273e08ab38cb
BertCasale <bthebest55@gmail.com>
2023-07-10 20:59:52 -0400

    sortWordsA function

--- solution.js
+++ solution.js
@@ -30,6 +30,15 @@ const sortNumsD = (arr) => {
 
 // sort words in ascending order case sensitive
 const sortWordsA = (arr) => {
+    for (let i = 1; i < arr.length; i++){
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > current){
+            [arr[j+1], arr[j]] = [arr[j], arr[j+1]]
+            j--;
+        }
+    }
     return arr;
 };
 
