6f8386e971b2b54d6e8ac67127f9262bb082fa95
BertCasale <bthebest55@gmail.com>
2023-07-10 20:57:07 -0400

    sortNumD function

--- solution.js
+++ solution.js
@@ -16,6 +16,15 @@ const sortNumsA = (arr) => {
 
 // sort numbers in descending order
 const sortNumsD = (arr) => {
+    for (let i = 1; i < arr.length; i++){
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] < current){
+            [arr[j+1], arr[j]] = [arr[j], arr[j+1]]
+            j--;
+        }
+    }
     return arr;
 };
 
