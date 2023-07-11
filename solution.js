e5587974064cc9f1aec7daa489e5cce9699e0c15
BertCasale <bthebest55@gmail.com>
2023-07-10 20:51:55 -0400

    sortNumsA function

--- solution.js
+++ solution.js
@@ -2,6 +2,15 @@ const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
 const sortNumsA = (arr) => {
+    for (let i = 1; i < arr.length; i++){
+        let current = arr[i];
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > current){
+            [arr[j+1], arr[j]] = [arr[j], arr[j+1]]
+            j--;
+        }
+    }
     return arr;
 };
 
