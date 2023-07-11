c80a6b29aaa65b934228e76b02bec350dfcef39b
christinacephus@pursuit.org <christinacephus@pursuit.org>
2023-07-10 23:26:29 -0400

    sortProductPriceA complete

--- solution.js
+++ solution.js
@@ -76,7 +76,19 @@ const sortProductNamesA = (someProducts) => {
 };
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = (someProducts) => {
+  for (let i = 1; i < someProducts.length; i++) {
+    let curr = someProducts[i];
+    let j = i - 1;
+    // sort products by name
+    while(j >= 0 && someProducts[j].price > curr.price) {
+      someProducts[j + 1] = someProducts[j];
+      j--;
+    }
+    someProducts[j + 1] = curr;
+  }
+  return someProducts;
+};
 
 // sort products by price, descending order
 const sortProductPriceD = () => {};
