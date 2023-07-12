7a2a93a93f5b8daf33207d5f954bb7bd0afb29e9
Wilsantos1975 <Faustosantos@pursuit.org>
2023-07-11 20:40:44 -0400

    working on solutions

--- solution.js
+++ solution.js
@@ -1,25 +1,46 @@
 const { catArt, someNums, someProducts, someWords } = require("./data/data.js");
 
 // sort numbers in ascending order
-const sortNumsA = () => {};
+const sortNumsA = (someNums) => {
+  return someNums.sort((a, b) => a - b);
+};
 
 // sort numbers in descending order
-const sortNumsD = () => {};
+const sortNumsD = (someNums) => {
+  return someNums.sort((a, b) => b - a);
+};
 
 // sort words in ascending order case sensitive
-const sortWordsA = () => {};
+const sortWordsA = () => {
+  return someWords.sort();
+};
 
 // sort words in descending order case insensitive
-const sortWordsD = () => {};
+const sortWordsD = () => {
+  return someWords.sort((a, b) =>
+    b.toLowerCase().localeCompare(a.toLowerCase())
+  );
+
+  
+};
 
 // sort products by name, ascending order case insensitive
-const sortProductNamesA = () => {};
+const sortProductNamesA = () => {
+  return someProducts.sort( (a, b) =>
+    a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+   
+  )
+};
 
 // sort products by price, ascending order
-const sortProductPriceA = () => {};
+const sortProductPriceA = () => {
+  return someProducts.sort((a, b) => a.price - b.price);
+};
 
 // sort products by price, descending order
-const sortProductPriceD = () => {};
+const sortProductPriceD = () => {
+  return someProducts.sort((a, b) => b.price - a.price); //
+};
 
 // sort products by price, then by name, ascending order
 const sortProducsPriceNameA = () => {};
