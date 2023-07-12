2beeea48c45fa50cc7ba831427acec858ec8dc5b
Wilsantos1975 <Faustosantos@pursuit.org>
2023-07-11 21:37:02 -0400

    lab finished, only bonus pending

--- solution.js
+++ solution.js
@@ -20,16 +20,13 @@ const sortWordsD = () => {
   return someWords.sort((a, b) =>
     b.toLowerCase().localeCompare(a.toLowerCase())
   );
-
-  
 };
 
 // sort products by name, ascending order case insensitive
 const sortProductNamesA = () => {
-  return someProducts.sort( (a, b) =>
+  return someProducts.sort((a, b) =>
     a.name.toLowerCase().localeCompare(b.name.toLowerCase())
-   
-  )
+  );
 };
 
 // sort products by price, ascending order
@@ -43,13 +40,37 @@ const sortProductPriceD = () => {
 };
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = () => {
+  // return someProducts.sort((a,b) => a.price - b.price).sort((a,b) =>
+  //   a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+  // )
+  return someProducts.sort((a, b) => {
+    if (a.price === b.price) {
+      return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+    }
+    return a.price - b.price;
+  });
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = () => {
+  return catArt.sort((a, b) =>
+    a.designedBy.toLowerCase().localeCompare(b.designedBy.toLowerCase())
+  );
+};
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = () => {
+  return catArt.sort((a, b) => {
+    a = a.price;
+    b = b.price;
+    if (typeof a === "string" && a.length === 1) a = Number(a);
+    if (typeof b === "string" && b.length === 1) b = Number(b);
+    if (typeof a === "string") a = Number(a[2]) * 10;
+    if (typeof b === "string") b = Number(b[2]) * 10;
+    return a - b;
+  });
+};
 
 // Create your own sort function
 // it should sort in ascending order
