adc7f7286c2e90b32957c19d0950ad0900d15832
Mark Robertson <markrobertson67@gmail.com>
2023-07-17 22:25:47 -0400

    10  passing

--- solution.js
+++ solution.js
@@ -105,22 +105,21 @@ const sortProductNamesA = (products) => {
 };
 
 // sort products by price, ascending order
-const sortProductPriceA = (price) => {
- 
-  for (let i = 1; i < price.length; i++) {
-    let current = price[i];
+const sortProductPriceA = (products) => {
+  for (let i = 1; i < products.length; i++) {
+    let current = products[i];
     let j = i - 1;
 
-    while (j >= 0 && price[j].price > current.price) {
-      price[j + 1] = price[j];
+    while (j >= 0 && products[j].price > current.price) {
+      products[j + 1] = products[j];
       j--;
     }
 
-    price[j + 1] = current;
+    products[j + 1] = current;
   }
-  return price;
-
+  return products;
 };
+
 
 // sort products by price, descending order
 const sortProductPriceD = (price) => {
@@ -140,13 +139,48 @@ const sortProductPriceD = (price) => {
 };
 
 // sort products by price, then by name, ascending order
-const sortProducsPriceNameA = () => {};
+const sortProducsPriceNameA = (products) => {
+  products.sort((a, b) => {
+    if (a.price === b.price) {
+      return a.name.localeCompare(b.name, undefined, { sensitivity: "case" });
+    } else {
+      return a.price - b.price;
+    }
+  });
+  return products;
+};
 
 // sort catArt by designed by
-const catArtSortDesginedByA = () => {};
+const catArtSortDesginedByA = (catArt) => {
+  catArt.sort((a, b) => a.designedBy.localeCompare(b.designedBy, undefined, { sensitivity: "case" }));
+  return catArt;
+};
+
 
 // sort catArt by price
-const catArtSortByPriceA = () => {};
+const catArtSortByPriceA = (catArt) => {
+  // Create a copy of the catArt array to avoid modifying the original data
+  const catArtCopy = catArt.map((item) => ({ ...item }));
+
+  // Convert prices to numeric values and round to two decimal places before sorting
+  catArtCopy.forEach((item) => {
+    if (typeof item.price === "string") {
+      // Remove non-numeric characters (like "♇") from the price string and parse as a float
+      item.price = parseFloat(item.price.replace(/[^0-9.-]/g, ""));
+    } else if (typeof item.price !== "number") {
+      // Set invalid price values to 0
+      item.price = 0;
+    }
+
+    // Round the price to two decimal places
+    item.price = parseFloat(item.price.toFixed(2));
+  });
+
+  catArtCopy.sort((a, b) => a.price - b.price);
+  return catArtCopy;
+};
+
+
 
 // Create your own sort function
 // it should sort in ascending order
@@ -156,9 +190,41 @@ const catArtSortByPriceA = () => {};
 // or try to implement merge sort
 // or look up another common sort algorithm (i.e. quicksort, ) and try your own implementation
 // Bonus add another argument that would allow the function to be used for ascending or descending order
-const mySortFunction = () => {
+const mySortFunction = (data, sortOrder = "asc") => {
+  // Convert data to an array (if it's not already an array)
+  const dataArray = Array.isArray(data) ? data : Object.values(data);
 
+  // Helper function to swap elements in the array
+  const swap = (arr, i, j) => {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  };
+
+  // Bubble sort algorithm
+  const bubbleSort = (arr) => {
+    const n = arr.length;
+    let sorted = false;
+
+    for (let i = 0; i < n - 1; i++) {
+      sorted = true;
+      for (let j = 0; j < n - i - 1; j++) {
+        if (sortOrder === "asc" ? arr[j] > arr[j + 1] : arr[j] < arr[j + 1]) {
+          swap(arr, j, j + 1);
+          sorted = false;
+        }
+      }
+
+      if (sorted) {
+        // If no swaps were made in this pass, the array is sorted, and we can break the loop.
+        break;
+      }
+    }
+
+    return arr;
+  };
+
+  return bubbleSort(dataArray);
 };
+
 
 module.exports = {
   sortNumsA,
